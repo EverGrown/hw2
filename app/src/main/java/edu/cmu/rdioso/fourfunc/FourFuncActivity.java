891cf56993aa078b7f0f42641b279f6956ec0008
Robert Dioso <Robert Dioso>
2017-02-01 21:45:52 +0000

    Calculator layout prototype

---
+++ app/src/main/java/edu/cmu/rdioso/fourfunc/FourFuncActivity.java
@@ -0,0 +1,13 @@
+package edu.cmu.rdioso.fourfunc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FourFuncActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four_func);
+    }
+}
